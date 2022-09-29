86b048a81aaa8c2efce09f6037bc8a5580c8e1ce
rohitbakde24 <bakderohit123@gmail.com>
2022-09-29 21:53:38 +0530

    first-server

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-const http = require('http')
+// const Circle = require("./Circle")
 
+// const circleObj = new Circle;
+// console.log(circleObj.area(5));
+// console.log(circleObj.circumference(5))
+const http = require('http')
+const PORT = 4000
 const server = http.createServer((req,res)=>{
     res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
     data: 'Hello World'
   }));
 })
+console.log(`server is running at http://localhost:${PORT}/`)
 
-
-server.listen(3000);
+server.listen(PORT);
---
+++ Circle.js
@@ -0,0 +1,9 @@
+class Circle {
+    area(r){
+        return Math.PI * r * r;
+    }
+    circumference(r){
+        return 2* Math.PI * r;
+    }
+}
+module.exports = Circle
